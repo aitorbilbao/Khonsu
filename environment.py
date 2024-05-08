2b533e20d74aa5d22354aee7e4ae1f307eb46e33
aitorbilbao <aitorbilbao25@gmail.com>
2024-05-08 16:47:34 +0200

    1

--- environment.py
+++ environment.py
@@ -1,13 +1,15 @@
 import gym
 import numpy as np
+import matplotlib.pyplot as plt
 
 from import_map import M, import_map_mesh
 
 X,Y,elevation = import_map_mesh(M)
 
 class MoonEnvironment(gym.Env):
-    def __init__(self): #add arguments
+    def __init__(self, X, Y, elevation): #add arguments
         super().__init__()
 
-
-
+        self.X = X
+        self.Y = Y
+        self.elevation = elevation
