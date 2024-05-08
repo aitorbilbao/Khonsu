dfabb8a11b07a352649786fc7ce0536950f92cba
aitorbilbao <aitorbilbao25@gmail.com>
2024-05-08 16:31:50 +0200

    a

--- environment.py
+++ environment.py
@@ -3,5 +3,11 @@ import numpy as np
 
 from import_map import M, import_map_mesh
 
-import_map_mesh(M)
+X,Y,elevation = import_map_mesh(M)
+
+class MoonEnvironment(gym.Env):
+    def __init__(self): #add arguments
+        super().__init__()
+
+
 
