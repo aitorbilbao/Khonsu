366191879ca1f81cf1e9997dd3c18b15cf482d2e
aitorbilbao <aitorbilbao25@gmail.com>
2024-05-14 13:45:14 +0200

    a

--- environment.py
+++ environment.py
@@ -2,10 +2,6 @@ import gym
 import numpy as np
 import matplotlib.pyplot as plt
 
-from import_map import M, import_map_mesh
-
-X,Y,elevation = import_map_mesh(M)
-
 class MoonEnvironment(gym.Env):
     def __init__(self, X, Y, elevation): #add arguments
         super().__init__()
@@ -13,3 +9,26 @@ class MoonEnvironment(gym.Env):
         self.X = X
         self.Y = Y
         self.elevation = elevation
+        self.map = np.array([self.X, self.Y])
+        self.map_size = self.map.shape
+        #frente, diagonal (2), lados (2), parado (1)
+        self.action_space = gym.spaces.Discrete(6)
+        self.observation_space = gym.spaces.Tuple((gym.spaces.Discrete(self.map_size[1]), gym.spaces.Discrete(self.map_size[2])))
+
+        #Initialize state
+        self.state = None
+
+    def reset(self):
+        #Reset to base
+        initial_position = [self.X[-1][len(self.X[-1]) // 2], self.Y[-1][len(self.Y[-1]) // 2]]
+        self.state = np.array(initial_position)
+        return self.state
+    
+    def step(self, action):
+        x,y = self.state
+
+        if action == 0: #Move forward
+            x =+ 1
+        
+
+        
--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 from stl import mesh
+import matplotlib.pyplot as plt
 from import_map import import_map_mesh, plot_grid
-
+from environment import MoonEnvironment
 
 # Download the STL file and load it into a mesh
 aitor = "C://Users//aitor//Desktop//Path AI//map.stl"
 # jan = 
 
-#Change file path
+#Change file path (mesh is 297x297 km)
 M = mesh.Mesh.from_file(aitor)
-grid_size = 50000
+grid_size = 10000
 
 
 X,Y,elevation = import_map_mesh(M,grid_size)
-plot_grid(X,Y,elevation)
+
+#plt.contourf(X, Y, elevation)
+#plt.colorbar()
+#plt.show()
+
+#print(Y)
+#plot_grid(X,Y,elevation)
+#env = MoonEnvironment(X,Y,elevation)
+#print(env.map)
