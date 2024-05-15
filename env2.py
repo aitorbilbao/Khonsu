2af2aa6da07ed68a1aef14c61fdf68dce829de17
aitorbilbao <aitorbilbao25@gmail.com>
2024-05-15 17:21:06 +0200

    :/

--- env2.py
+++ env2.py
@@ -12,24 +12,30 @@ class MoonEnvironment(gym.Env):
         self.X = X
         self.Y = Y
         self.elevation = elevation
+        #Initial position = base, Goal1 = Big crater. It works with indices.
         self.initial_position = [len(self.X)//2, len(self.Y)//2]
-        self.goal_position = [4*len(self.X)//6, 4*len(self.Y)//6]
+        """"
+        TODO: We have to change the goal position to the big crater, and add the rest
+        """
+        self.goal1_position = [4*len(self.X)//6, 4*len(self.Y)//6]
         self.state = self.initial_position
         
-        self.action_space = gym.spaces.Discrete(4)
+        # Possible actions: 5
+        self.action_space = gym.spaces.Discrete(5)
         self.observation_space = gym.spaces.Tuple((gym.spaces.Discrete(len(self.X)), gym.spaces.Discrete(len(self.Y))))
 
     def step(self, action):
-        if action == 0 and self.state< self.Y[-1][-1]:  # up
+        if action == 0 and self.state[1] < len(self.Y)-1:  # up
             self.state[1] += 1
         elif action == 1 and self.state[1] > 0:  # down
             self.state[1] -= 1
         elif action == 2 and self.state[0] > 0:  # left
             self.state[0] -= 1
-        elif action == 3 and self.state[0]<self.X[-1][-1]:  # right
+        elif action == 3 and self.state[0] < len(self.X)-1:  # right
             self.state[0] += 1
     
-        done = self.state == self.goal_position
+        done = self.state == self.goal1_position
+        """TODO: Add rewards and extra conditions"""
         return np.array(self.state), 0.0, done, {}
     
     def reset(self):
@@ -39,5 +45,5 @@ class MoonEnvironment(gym.Env):
     def render(self):
         plt.imshow(self.elevation, cmap='Spectral', origin='lower')
         plt.scatter(*self.state, color='red')
-        plt.scatter(*self.goal_position, color='green')
+        plt.scatter(*self.goal1_position, color='green')
         plt.pause(0.001)  # pause a bit so that plots are updated
--- main.py
+++ main.py
@@ -11,7 +11,7 @@ aitor = "C://Users//aitor//Desktop//Path AI//map.stl"
 
 #Change file path (mesh is 297x297 km)
 M = mesh.Mesh.from_file(aitor)
-grid_size = 10000
+grid_size = 50000
 
 
 X,Y,elevation = import_map_mesh(M,grid_size)
@@ -20,14 +20,14 @@ X,Y,elevation = import_map_mesh(M,grid_size)
 #self.cell_size = 1
 #self.screen = pygame.display.set_mode((self.map_size[1]*self.cell_size, self.map_size[2]*self.cell_size))
 #print(Y)
-plot_grid(X,Y,elevation)
+#plot_grid(X,Y,elevation)
 #print(env.map)
 #plt.contourf(X, Y, elevation)
 #plt.colorbar()
 #plt.show()
 
-#env = MoonEnvironment(X,Y,elevation)
+env = MoonEnvironment(X,Y,elevation)
 
-#while True:
+#hile True:
 #    env.render()
 # ---------------------------------------------------------
