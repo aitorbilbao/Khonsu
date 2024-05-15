8f6607118a46bc1d0fbb577d0fd346bbc304b57f
aitorbilbao <aitorbilbao25@gmail.com>
2024-05-15 18:54:42 +0200

    update

--- env_my.py
+++ env_my.py
@@ -25,6 +25,7 @@ class MoonEnvironment(gym.Env):
         self.observation_space = gym.spaces.Tuple((gym.spaces.Discrete(len(self.X)), gym.spaces.Discrete(len(self.Y))))
 
     def step(self, action):
+        self.old_state = self.state
         if action == 0 and self.state[1] < len(self.Y)-1:  # up
             self.state[1] += 1
         elif action == 1 and self.state[1] > 0:  # down
@@ -33,17 +34,28 @@ class MoonEnvironment(gym.Env):
             self.state[0] -= 1
         elif action == 3 and self.state[0] < len(self.X)-1:  # right
             self.state[0] += 1
-    
+        elif action == 4:  # stay still
+            pass
+        
+        if self.state == self.old_state:
+            return np.array(self.state), -100, False, {}  # large negative reward for invalid move
+
+        #Elevation cost (going down is good, going up is bad)
+        elevation_cost = self.elevation[self.old_state[0], self.old_state[1]] - self.elevation[self.state[0], self.state[1]]
+        
         done = self.state == self.goal1_position
+        reward = -1 + elevation_cost
+        if done:
+            reward = 1000000000
         """TODO: Add rewards and extra conditions"""
-        return np.array(self.state), 0.0, done, {}
+        return np.array(self.state), reward, done, {}
     
     def reset(self):
         self.state = self.initial_position
         return np.array(self.state)
     
     def render(self):
-        plt.imshow(self.elevation, cmap='Spectral', origin='lower')
+        plt.imshow(self.elevation, cmap='terrain', origin='lower')
         plt.scatter(*self.state, color='red')
         plt.scatter(*self.goal1_position, color='green')
         plt.pause(0.001)  # pause a bit so that plots are updated
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from stl import mesh
 import matplotlib.pyplot as plt
 from import_map import import_map_mesh, plot_grid
-from env2 import MoonEnvironment
+from env_my import MoonEnvironment
 import keyboard
 import pygame
 
@@ -11,10 +11,12 @@ aitor = "C://Users//aitor//Desktop//Path AI//map.stl"
 
 #Change file path (mesh is 297x297 km)
 M = mesh.Mesh.from_file(aitor)
-grid_size = 50000
+grid_size = 10000
 
 
 X,Y,elevation = import_map_mesh(M,grid_size)
+
+
 
 #pygame.init()
 #self.cell_size = 1
@@ -26,8 +28,23 @@ X,Y,elevation = import_map_mesh(M,grid_size)
 #plt.colorbar()
 #plt.show()
 
-env = MoonEnvironment(X,Y,elevation)
-
-#hile True:
-#    env.render()
 # ---------------------------------------------------------
+
+
+import numpy as np
+
+# Create an instance of the environment
+env = MoonEnvironment(X, Y, elevation)
+
+# Reset the environment and get the initial state
+state = env.reset()
+
+# Run an episode
+done = False
+while not done:
+    # Select an action
+    action = np.random.choice(env.action_space.n)  # random policy
+    # Take a step in the environment
+    state, reward, done, info = env.step(action)
+    # Render the environment
+    env.render()
